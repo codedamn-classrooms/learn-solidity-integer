cb4f9cb1db62c3cf03bd78b37839349ef61515f3
kshitijofficial <52281964+kshitijofficial@users.noreply.github.com>
2022-06-21 19:14:11 +0530

    Update Integer.sol

--- contracts/Integer.sol
+++ contracts/Integer.sol
@@ -3,9 +3,9 @@ pragma solidity >=0.5.0 <0.9.0;
 
 contract Integer {
     //Identify the odd one out
-    //uint8 a = -1; //odd as uint type variable cannot store negative numbers
-   // uint16 b = 255;
-    //int8 c = -129; //-129 is not in the range
-   //int16 d = 0;
-   // uint32 e = 4294967295;
+    uint8 a = -1; 
+   uint16 b = 255;
+    int8 c = -129; 
+   int16 d = 0;
+    uint32 e = 4294967295;
 }
